b9b892b7ff8e510061ebd974308777d612b0f987
sumityadav555 <sumityaadav550@gmail.com>
2025-01-31 05:29:22 +0000

    dict method

---
+++ dictinoary.py
@@ -0,0 +1,9 @@
+# dict is collection data type in which it stored value in the pair of key
+# and value.
+
+# Retrieves the value of a key. If the key does not exist, it return the default
+# value
+
+# person = {'name': "Alice", 'age': 25}
+# print(person.get("name"))
+# print(person.get('sumit', -1))
